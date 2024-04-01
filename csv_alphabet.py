9306580eab2c86ac56b5975d0bcaad1e10c63caf
Peter Hosey <git@boredzo.org>
2024-04-01 16:40:48 -0700

    Begin adding options to control how alphabets are computed. The default behavior is now the --all option (i.e., select combinations of values across all indicated columns).

--- csv_alphabet.py
+++ csv_alphabet.py
@@ -9,11 +9,15 @@ import locale
 
 locale.setlocale(locale.LC_ALL, '')
 
+class ColumnMode(str):
+	pass
+MODE_ALL = ColumnMode('all')
+
 def get_from_indexes(orig_row, indexes):
 	permuted_row = [ orig_row[i] for i in indexes ]
 	return permuted_row
 
-def select_distinct(input_path: pathlib.Path, columns_of_interest: list):
+def select_distinct(input_path: pathlib.Path, columns_of_interest: list, mode: ColumnMode):
 	found_values = set()
 
 	with open(input_path, 'r') as input_file:
@@ -21,15 +25,17 @@ def select_distinct(input_path: pathlib.Path, columns_of_interest: list):
 		header = next(reader)
 
 		indexes = [ header.index(col) for col in columns_of_interest ]
-		
-		for orig_row in reader:
-			permuted_row = get_from_indexes(orig_row, indexes)
-			found_values.add(tuple(permuted_row))
+
+		if mode == MODE_ALL:
+			for orig_row in reader:
+				permuted_row = get_from_indexes(orig_row, indexes)
+				found_values.add(tuple(permuted_row))
 
 	return found_values
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
+	parser.add_argument('--all', action='store_const', const=MODE_ALL, dest='mode', default=MODE_ALL, help='Return combinations of all columns. This is the default.')
 	parser.add_argument('--column', action='append', dest='column_names', help="Column to collect distinct values from.")
 	parser.add_argument('input_paths', type=pathlib.Path, nargs='+', help="Paths to files containing CSV data to inventory.")
 	opts = parser.parse_args()
@@ -37,7 +43,7 @@ if __name__ == "__main__":
 	all_combos = set()
 	num_combos = 0
 	for input_path in opts.input_paths:
-		these_combos = select_distinct(input_path, opts.column_names)
+		these_combos = select_distinct(input_path, opts.column_names, opts.mode)
 		all_combos |= these_combos
 
 	writer = csv.writer(sys.stdout)
