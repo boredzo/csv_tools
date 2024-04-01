e5ce153720f6a2e0aa35c1f00f53dbdca0ad87e1
Peter Hosey <git@boredzo.org>
2024-04-01 16:50:24 -0700

    Add the option for --any to select any value from any column of interest and return the union of them.
    As part of this, missing columns are no longer an error.

--- csv_alphabet.py
+++ csv_alphabet.py
@@ -11,6 +11,7 @@ locale.setlocale(locale.LC_ALL, '')
 
 class ColumnMode(str):
 	pass
+MODE_ANY = ColumnMode('any')
 MODE_ALL = ColumnMode('all')
 
 def get_from_indexes(orig_row, indexes):
@@ -24,18 +25,31 @@ def select_distinct(input_path: pathlib.Path, columns_of_interest: list, mode: C
 		reader = csv.reader(input_file)
 		header = next(reader)
 
-		indexes = [ header.index(col) for col in columns_of_interest ]
+		indexes = []
+		for col in columns_of_interest:
+			try:
+				idx = header.index(col)
+			except ValueError:
+				pass
+			else:
+				indexes.append(idx)
 
 		if mode == MODE_ALL:
 			for orig_row in reader:
 				permuted_row = get_from_indexes(orig_row, indexes)
 				found_values.add(tuple(permuted_row))
+		elif mode == MODE_ANY:
+			for orig_row in reader:
+				permuted_row = get_from_indexes(orig_row, indexes)
+				for value in permuted_row:
+					found_values.add((value,))
 
 	return found_values
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--all', action='store_const', const=MODE_ALL, dest='mode', default=MODE_ALL, help='Return combinations of all columns. This is the default.')
+	parser.add_argument('--any', action='store_const', const=MODE_ANY, dest='mode', default=MODE_ALL, help='Return all values from any column—that is, the union of all columns.')
 	parser.add_argument('--column', action='append', dest='column_names', help="Column to collect distinct values from.")
 	parser.add_argument('input_paths', type=pathlib.Path, nargs='+', help="Paths to files containing CSV data to inventory.")
 	opts = parser.parse_args()
