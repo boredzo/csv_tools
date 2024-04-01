bfca4cb4e0b93eaab9e3a4297e2620fe7a46dffd
Peter Hosey <git@boredzo.org>
2024-04-01 16:20:06 -0700

    New tool to return the alphabet for a column (like `SELECT DISTINCT` in SQL).

---
+++ csv_alphabet.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import pathlib
+import argparse
+import csv
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
+
+def get_from_indexes(orig_row, indexes):
+	permuted_row = [ orig_row[i] for i in indexes ]
+	return permuted_row
+
+def has_nonempty_values(seq):
+	"Iterate through the iterable seq and return True if any non-empty (truthy) value is encountered. Return False if not."
+	for x in seq:
+		if x:
+			return True
+	else:
+		return False
+
+def select_distinct(input_path: pathlib.Path, column_of_interest: str):
+	found_values = set()
+
+	with open(input_path, 'r') as input_file:
+		reader = csv.reader(input_file)
+		header = next(reader)
+		idx = header.index(column_of_interest)
+		
+		for orig_row in reader:
+			found_values.add(orig_row[idx])
+
+	return found_values
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('column_name', help="Column to collect distinct values from.")
+	parser.add_argument('input_paths', type=pathlib.Path, nargs='+', help="Paths to files containing CSV data to inventory.")
+	opts = parser.parse_args()
+
+	all_values = set()
+	for input_path in opts.input_paths:
+		num_values = 0
+		these_values = select_distinct(input_path, opts.column_name)
+		all_values |= these_values
+	for value in all_values:
+		print(value)
+		num_values += 1
+	print('{}\t{:n}'.format('total', num_values), file=sys.stderr)
