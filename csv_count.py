6dfc743784e6538f1dd2b867e4c2f6d296a1d9f9
Peter Hosey <git@boredzo.org>
2024-08-17 21:45:23 -0700

    Add support for counting rows from stdin. Also corrected the help while at it.

--- csv_count.py
+++ csv_count.py
@@ -6,30 +6,39 @@ import pathlib
 import argparse
 import csv
 
-def count_records(input_path):
+def count_records(input_file):
 	row_count = 0
 
-	with open(input_path, 'r') as input_file:
-		reader = csv.reader(input_file)
-		header = next(reader)
+	reader = csv.reader(input_file)
+	header = next(reader)
 
-		for row in reader:
-			row_count += 1
+	for row in reader:
+		row_count += 1
 
 	return row_count
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument('input_paths', type=pathlib.Path, nargs='+', help="Path to one or more files containing CSV data to concatenate into one large file. The first file's header determines the schema of all others; any files with a different header will be skipped.")
+	parser.add_argument('input_paths', type=pathlib.Path, nargs='*', help="Path to one or more files containing CSV data to count rows of. If omitted, read from stdin.")
 	opts = parser.parse_args()
 
-	total_row_count = 0
-	for path in opts.input_paths:
-		row_count = count_records(path)
+	if opts.input_paths:
+		total_row_count = 0
+		for path in opts.input_paths:
+			with open(input_path, 'r') as input_file:
+				row_count = count_records(input_file)
+				if len(opts.input_paths) > 1:
+					print('{}\t{:n}'.format(path, row_count))
+				else:
+					print('{:n}'.format(row_count))
+				total_row_count += row_count
+		print('{}\t{:n}'.format('total', total_row_count))
+	else:
+		path = '-'
+		input_file = sys.stdin
+
+		row_count = count_records(input_file)
 		if len(opts.input_paths) > 1:
 			print('{}\t{:n}'.format(path, row_count))
 		else:
 			print('{:n}'.format(row_count))
-		total_row_count += row_count
-	if len(opts.input_paths) > 1:
-		print('{}\t{:n}'.format('total', total_row_count))
