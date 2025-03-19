95e7bc1d966ba2932f47ef61965194b4bb0c101a
Peter Hosey <git@boredzo.org>
2025-03-18 20:38:56 -0700

    Fix #1: csv_partition: Add an option for random perturbations in row count per segment.

--- csv_partition.py
+++ csv_partition.py
@@ -7,8 +7,71 @@ import argparse
 import fileinput
 import csv
 import string
+import itertools
+import math
 import locale
 locale.setlocale(locale.LC_ALL, '')
+
+def factors(n):
+	max_factor = int(math.sqrt(n))
+	factors = set()
+	for divisor in range(2, max_factor + 1):
+		if n % divisor == 0:
+			factors.add(divisor)
+			factors.add(n // divisor)
+	return factors
+
+def avg(nums):
+	if not nums:
+		return 0
+	return sum(nums) / len(nums)
+
+def avg_factor(n):
+	divs = factors(n)
+	mean = int(avg(divs))
+	offset = 0
+	while mean + offset in divs and mean - offset in divs and mean - offset > 0:
+		offset += 1
+	if mean + offset not in divs:
+		return mean + offset
+	if mean - offset not in divs:
+		return mean - offset
+	return None
+
+def find_stride_length(n):
+	return (avg_factor(n) or int(math.sqrt(n))) + int(math.sqrt(n))
+
+def test_factors():
+	print(factors(24))
+
+def test_avg_factors():
+	print(avg_factor(24))
+
+class RandomRange:
+	"A sequence of integers that behaves like a range, but generates numbers in a pseudorandom order rather than linear order."
+	def __init__(self, start=0, stop=None, step=1):
+		self._pool = list(range(start, stop, step))
+		self._num_yielded = 0
+		self._num_available = len(self._pool)
+		self._stride_length = find_stride_length(self._num_available)
+		self._cur_idx = int(math.sqrt(self._num_available))
+	def __len__(self):
+		return self._num_available
+	def __iter__(self):
+		return self
+	def __next__(self):
+		if (self._num_yielded >= self._num_available):
+			raise StopIteration
+		next_item = self._pool[self._cur_idx]
+		self._cur_idx = (self._cur_idx + self._stride_length) % self._num_available
+		self._num_yielded += 1
+		return next_item
+
+def test_randomrange():
+	rr = RandomRange(1, 25+1)
+	idxs = list(rr)
+	print(len(rr), len(idxs))
+	print(idxs, file=sys.stderr)
 
 def column_segment_identifier(column_segment_number: int):
 	"Return strings like A, B, C, … Z, AA, AB, AC…."
@@ -133,6 +196,12 @@ def apply_renames(orig_header: list, renames: dict):
 def segment(input_path: pathlib.Path, opts: argparse.Namespace):
 	rows_per_file = opts.rows_per_file
 	rows_so_far = 0
+	rows_so_far_this_segment = 0
+
+	if opts.perturb_output_count and rows_per_file > 100:
+		perturbations = itertools.cycle(RandomRange(1, 100))
+	else:
+		perturbations = itertools.repeat(0)
 
 	reader = csv.reader(open(input_path, 'r', encoding=opts.input_encoding))
 	orig_header = next(reader)
@@ -140,13 +209,13 @@ def segment(input_path: pathlib.Path, opts: argparse.Namespace):
 	if not permutations:
 		sys.exit('No columns to include in output (original column set: {!r})'.format(orig_header))
 
-	def open_files(row_segment_number):
+	def open_files(row_segment_number, rows_this_segment):
 		out_files = []
 		writers = []
 
 		for column_segment_number, indexes in enumerate(permutations):
 			output_path = output_path_for_input_path(input_path, column_segment_number, row_segment_number, opts)
-			print('Writing up to {:n} rows to {}'.format(rows_per_file, output_path))
+			print('Writing up to {:n} rows to {}'.format(rows_this_segment, output_path))
 			output_file = open(output_path, 'w')
 			writer = csv.writer(output_file)
 
@@ -160,21 +229,26 @@ def segment(input_path: pathlib.Path, opts: argparse.Namespace):
 		return out_files, writers
 
 	row_segment_number = 1
-	out_files, writers = open_files(row_segment_number)
+	max_rows_this_segment = rows_per_file - next(perturbations)
+	out_files, writers = open_files(row_segment_number, max_rows_this_segment)
 
 	for row in reader:
-		if rows_per_file != 0 and rows_so_far > 0 and rows_so_far % rows_per_file == 0:
-			print('Wrote {:n} rows'.format(rows_so_far))
+		if max_rows_this_segment != 0 and rows_so_far_this_segment > 0 and rows_so_far_this_segment % max_rows_this_segment == 0:
+			print('Wrote {:n} rows'.format(rows_so_far_this_segment))
 			for f in out_files: f.close()
 
 			row_segment_number += 1
-			out_files, writers = open_files(row_segment_number)
+			rows_so_far_this_segment = 0
+			perturb = next(perturbations)
+			max_rows_this_segment = rows_per_file - perturb
+			out_files, writers = open_files(row_segment_number, max_rows_this_segment)
 
 		for column_segment_number, indexes in enumerate(permutations):
 			w = writers[column_segment_number]
 			segment = get_from_indexes(row, indexes)
 			w.writerow(segment)
 
+		rows_so_far_this_segment += 1
 		rows_so_far += 1
 	else:
 		print('Wrote a total of {:n} rows'.format(rows_so_far))
@@ -194,6 +268,7 @@ def main():
 	parser.add_argument('-n', '--rows-per-file', type=int, default=0, help='Split the input into segments of this many rows each.')
 	parser.add_argument('-o', '--output-directory', default=None, type=pathlib.Path, help='Directory in which output files are created. Defaults to the same directory as each input file.')
 	parser.add_argument('--output-filename-format', default='{basename}-pt{row_segment:04}-{column_segment}', help='Format for the names under which output segment files will be created. Row segments are numbers starting from 1. Column segments are letters starting from A.')
+	parser.add_argument('--perturb-output-count', default=False, action='store_true', help='If true, remove a small random number of rows from each vertical segment. (The number of rows will remain constant across horizontal segments.) This can be used to distinguish segments after transformations, uploads, etc.')
 	parser.add_argument('input_paths', type=pathlib.Path, nargs='+', help='CSV files to read. Each file gets split separately; all segments of one input file can be re-joined to reproduce that file.')
 	opts = parser.parse_args()
 
