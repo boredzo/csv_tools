b93c7f00779d652e06a3e648278b022046d25d31
Peter Hosey <git@boredzo.org>
2025-11-19 13:42:20 -0800

    csv_lint: Beefed up the reporting of both overflow and underflow errors.

--- csv_lint.py
+++ csv_lint.py
@@ -21,6 +21,9 @@ class Diagnostic:
 	def record_as_found_on_row(self, row_number: int, row: list):
 		self.range = range(self.range.start, row_number + 1)
 		self.specimens[row_number] = row
+	def last_row(self):
+		row_idx = self.range.stop - 1
+		return row_idx, self.specimens[row_idx]
 
 	def __len__(self):
 		return len(self.range)
@@ -37,41 +40,78 @@ class Diagnostic:
 		if self.message is None: return
 		print(str(self), file=sys.stderr)
 		if verbose:
-			self.print_specimens()
+#			self.print_specimens()
+			self.flush_verbose()
+	def flush_verbose(self):
+		pass
 
 	def print_specimens(self, max_specimens=3):
 		for i, row_idx in enumerate(self.range):
 			if i >= max_specimens: break
 			row = self.specimens[row_idx]
 
+class DiagnosticColumnUnderflow(Diagnostic):
+	def __init__(self, first_row_number: int, message: str, header: list):
+		super().__init__(first_row_number, message)
+		self.header = header
+
+	def flush_verbose(self):
+		row_idx, row = self.last_row()
+		print('Example for row #{}:'.format(row_idx))
+		for col_idx, column, value in zip(range(len(row)), self.header, row):
+			print('{}\t{}\t{}'.format(col_idx, column, value or '(empty)'))
+
+class DiagnosticColumnOverflow(Diagnostic):
+	def __init__(self, first_row_number: int, message: str, header: list):
+		super().__init__(first_row_number, message)
+		self.header = header
+
+	def flush_verbose(self):
+		row_idx, row = self.last_row()
+		print('Example for row #{}:'.format(row_idx))
+		extension = [ '???' ] * (len(row) - len(self.header))
+		for col_idx, column, value in zip(range(len(row)), self.header + extension, row):
+			print('{}\t{}\t{}'.format(col_idx, column, value or '(empty)'))
+
 def lint_reader(reader: csv.reader, header: list, verbose: bool):
 	global g_row_count
 
 	expected_column_count = len(header)
-	last_diagnostic = Diagnostic(0, None)
+	if verbose:
+		print('Expecting {:n} columns per row'.format(expected_column_count))
+	last_diagnostic = None
 
 	for row in reader:
 		g_row_count += 1
 		this_column_count = len(row)
+
+		if verbose and g_row_count == 1:
+			print('First row has {:n} columns'.format(this_column_count))
+
 		if this_column_count < expected_column_count:
 			message = 'Column underflow: Expected {:n} columns, got {:n}'.format(expected_column_count, this_column_count)
-			if last_diagnostic.message != message:
-				last_diagnostic.flush()
-				if verbose:
-					for col_idx, column, value in zip(range(len(row)), header, row):
-						print('{}\t{}\t{}'.format(col_idx, column, value or '(empty)'))
+
+			if last_diagnostic is None:
+				last_diagnostic = DiagnosticColumnUnderflow(g_row_count, message, header)
+			elif last_diagnostic.message != message:
+				last_diagnostic.flush(verbose=verbose)
 				last_diagnostic = Diagnostic(g_row_count, message)
+
 			last_diagnostic.record_as_found_on_row(g_row_count, row)
+
 		elif this_column_count > expected_column_count:
 			message = 'Column overflow: Expected {:n} columns, got {:n}'.format(expected_column_count, this_column_count)
-			if last_diagnostic.message != message:
-				last_diagnostic.flush()
-				if verbose:
-					extension = [ '???' ] * (len(row) - len(header))
-					for row_idx, column, value in zip(range(len(row)), header + extension, row):
-						print('{}\t{}\t{}'.format(row_idx, column, value or '(empty)'))
+
+			if last_diagnostic is None:
+				last_diagnostic = DiagnosticColumnOverflow(g_row_count, message, header)
+			elif last_diagnostic.message != message:
+				last_diagnostic.flush(verbose=verbose)
 				last_diagnostic = Diagnostic(g_row_count, message)
+
 			last_diagnostic.record_as_found_on_row(g_row_count, row)
+
+	if last_diagnostic is not None:
+		last_diagnostic.flush()
 
 	return g_row_count
 
