842f2f644faf08d4acf6535ec13b016a0d94e3ff
Peter Hosey <git@boredzo.org>
2026-06-24 19:32:00 -0700

    Added a test for csv_histo testing the counting of only certain columns.

--- tests/test_histo.py
+++ tests/test_histo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import pathlib
+import csv
 import subprocess
 import unittest
 
@@ -28,6 +29,24 @@ class TestHistogram(unittest.TestCase):
 		self.assertTrue(gotUniques)
 		self.assertTrue(gotRowsCounted)
 		self.assertTrue(gotAllRows)
+		self.assertEqual(csv_histo.wait(), 0)
+
+	def test_multipleMatches(self):
+		csv_histo = subprocess.Popen([ 'bin/csv_histo.py', '--only-columns=first,last', 'test_data/csv_histo_test.csv' ], stdout=subprocess.PIPE, stderr=open('/dev/null', 'w'), text=True)
+		expected = {
+			('John', 'Egbert'): 1,
+			('Rose', 'Lalonde'): 1,
+			('Dave', 'Strider'): 3,
+			('Jade', 'Harley'): 2,
+		}
+		reader = csv.reader(csv_histo.stdout)
+		next(reader)
+		for row in reader:
+			key = tuple(row[1:])
+			expected_count = expected[key]
+			actual_count = int(row[0])
+			self.assertEqual(expected_count, actual_count, 'incorrect count for {!r}'.format(key))
+
 		self.assertEqual(csv_histo.wait(), 0)
 
 if __name__ == "__main__":
